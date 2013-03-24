c86a922eed5db82e29c543517e3eeb6d74b76f70
Sergei Lebedev <superbobry@gmail.com>
2013-03-24 17:21:07 +0400

    'Data.BitSet.Dynamic' now uses a newtype wrapper

--- src/Data/BitSet/Dynamic.hs
+++ src/Data/BitSet/Dynamic.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE MagicHash #-}
-{-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.BitSet.Dynamic
     (
@@ -42,18 +40,57 @@ module Data.BitSet.Dynamic
 
 import Prelude hiding (null)
 
-import Data.Bits ((.&.), (.|.), setBit, clearBit, testBit)
-import Data.List (foldl')
-import GHC.Base (Int(..), Word(..), divModInt#)
+import Data.Bits (Bits(..))
+import GHC.Base (Int(..), divInt#, modInt#)
 import GHC.Exts (popCnt#)
 import GHC.Integer.GMP.Internals (Integer(..))
 import GHC.Prim (Int#, Word#, (+#), (==#), (>=#),
                  word2Int#, int2Word#, plusWord#, indexWordArray#)
+import GHC.Word (Word(..))
+
+import Control.DeepSeq (NFData(..))
 
 import Data.BitSet.Types (GBitSet(..))
 import qualified Data.BitSet as BS
 
-type BitSet = GBitSet Integer
+newtype FasterInteger = FasterInteger { unFI :: Integer }
+       deriving (Read, Show, Eq, Ord, Enum, Integral, Num, Real,
+                 NFData)
+
+instance Bits FasterInteger where
+    FasterInteger x .&. FasterInteger y = FasterInteger $ x .&. y
+    {-# INLINE (.&.) #-}
+
+    FasterInteger x .|. FasterInteger y = FasterInteger $ x .|. y
+    {-# INLINE (.|.) #-}
+
+    FasterInteger x `xor` FasterInteger y = FasterInteger $ x `xor` y
+    {-# INLINE xor #-}
+
+    complement = FasterInteger . complement . unFI
+    {-# INLINE complement #-}
+
+    shift (FasterInteger x) i = FasterInteger $ shift x i
+    {-# INLINE shift #-}
+
+    rotate (FasterInteger x) i = FasterInteger $ rotate x i
+    {-# INLINE rotate #-}
+
+    bit = FasterInteger . bit
+    {-# INLINE bit #-}
+
+    testBit (FasterInteger x) i = testBitInteger x i
+    {-# INLINE testBit #-}
+
+    popCount = popCountInteger . unFI
+
+    bitSize = bitSize . unFI
+    {-# INLINE bitSize #-}
+
+    isSigned = isSigned . unFI
+    {-# INLINE isSigned #-}
+
+type BitSet = GBitSet FasterInteger
 
 -- | /O(1)/. Is the bit set empty?
 null :: BitSet a -> Bool
@@ -67,12 +104,12 @@ size = BS.size
 
 -- | /O(1)/. Ask whether the item is in the bit set.
 member :: a -> BitSet a -> Bool
-member x (BitSet { _bits }) = _bits `testBitInteger` fromEnum x
+member = BS.member
 {-# INLINE member #-}
 
 -- | /O(1)/. Ask whether the item is in the bit set.
 notMember :: a -> BitSet a -> Bool
-notMember x = not . member x
+notMember = BS.notMember
 {-# INLINE notMember #-}
 
 -- | /O(max(n, m))/. Is this a subset? (@s1 isSubsetOf s2@) tells whether
@@ -96,28 +133,17 @@ singleton = BS.singleton
 
 -- | /O(1)/. Insert an item into the bit set.
 insert :: a -> BitSet a -> BitSet a
-insert x bs@(BitSet { _bits }) =
-    bs { _n = popCntInteger b, _bits = b }
-  where
-    b :: Integer
-    b = setBit _bits $ fromEnum x
+insert = BS.insert
 {-# INLINE insert #-}
 
 -- | /O(1)/. Delete an item from the bit set.
 delete :: a -> BitSet a -> BitSet a
-delete x bs@(BitSet { _bits }) =
-    bs { _n = popCntInteger b, _bits = b }
-  where
-    b :: Integer
-    b = clearBit _bits $ fromEnum x
+delete = BS.delete
 {-# INLINE delete #-}
 
 -- | /O(max(m, n))/. The union of two bit sets.
 union :: BitSet a -> BitSet a -> BitSet a
-union (BitSet { _bits = b1 }) (BitSet { _bits = b2 }) =
-    BitSet { _n = popCntInteger b, _bits = b }
-  where
-    b = b1 .|. b2
+union = BS.union
 {-# INLINE union #-}
 
 -- | /O(max(m, n))/. The union of a list of bit sets.
@@ -136,10 +162,7 @@ difference = BS.difference
 
 -- | /O(max(m, n))/. The intersection of two bit sets.
 intersection :: BitSet a -> BitSet a -> BitSet a
-intersection (BitSet { _bits = b1 }) (BitSet { _bits = b2 }) =
-    BitSet { _n = popCntInteger b, _bits = b }
-  where
-    b = b1 .&. b2
+intersection = BS.intersection
 {-# INLINE intersection #-}
 
 -- | /O(n)/. Convert the bit set set to a list of elements.
@@ -153,17 +176,17 @@ elems = BS.toList
 
 -- | /O(n)/. Make a bit set from a list of elements.
 fromList :: Enum a => [a] -> BitSet a
-fromList xs = BitSet { _n = popCntInteger b, _bits = b } where
-  b = foldl' (\i x -> setBit i (fromEnum x)) 0 xs
+fromList = BS.fromList
 {-# INLINE fromList #-}
 
 
-popCntInteger :: Integer -> Int
-popCntInteger x = I# (word2Int# (popCntInteger# x))
+popCountInteger :: Integer -> Int
+popCountInteger x = I# (word2Int# (popCountInteger# x))
+{-# INLINE popCountInteger #-}
 
-popCntInteger# :: Integer -> Word#
-popCntInteger# (S# i#)    = popCnt# (int2Word# i#)
-popCntInteger# (J# s# d#) = go 0# (int2Word# 0#) where
+popCountInteger# :: Integer -> Word#
+popCountInteger# (S# i#)    = popCnt# (int2Word# i#)
+popCountInteger# (J# s# d#) = go 0# (int2Word# 0#) where
   go i acc =
       if i ==# s#
       then acc
@@ -175,11 +198,16 @@ popCntInteger# (J# s# d#) = go 0# (int2Word# 0#) where
 testBitInteger :: Integer -> Int -> Bool
 testBitInteger (S# i#) b = I# i# `testBit` b
 testBitInteger (J# s# d#) (I# b#) =
-    if block >=# s#
+    if block# >=# s#
     then False
-    else W# (indexWordArray# d# block) `testBit` I# offset
+    else W# (indexWordArray# d# block#) `testBit` I# offset#
   where
     n# :: Int#
     n# = WORD_SIZE_IN_BITS#
 
-    (# block, offset #) = b# `divModInt#` n#
+    block# :: Int#
+    block# = b# `divInt#` n#
+
+    offset# :: Int#
+    offset# = b# `modInt#` n#
+{-# INLINE testBitInteger #-}
