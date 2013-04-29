3ff0770460d1d94e2323bf042cee11f8556ea637
Sergei Lebedev <superbobry@gmail.com>
2013-04-29 17:35:19 +0400

    Added Cabal magic for generating 'GmpDerivedConstants.h'

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,35 @@
 #!/usr/bin/env runhaskell
+{-# LANGUAGE NamedFieldPuns #-}
+{-# OPTIONS_GHC -Wall -Werror #-}
 
-import Distribution.PackageDescription (HookedBuildInfo)
-import Distribution.Simple (UserHooks(..), Args,
+import System.FilePath ((</>))
+
+import Distribution.PackageDescription (PackageDescription)
+import Distribution.Simple (UserHooks(..),
                             defaultMainWithHooks, simpleUserHooks)
-import Distribution.Simple.Setup (BuildFlags(..))
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
+import Distribution.Simple.Program (gccProgram, lookupProgram, runProgram)
+import Distribution.Simple.Setup (BuildFlags)
+import Distribution.Simple.Utils (die, withTempFile, rawSystemStdout,
+                                  writeFileAtomic)
+import Distribution.Verbosity (silent)
 
 main :: IO ()
 main = defaultMainWithHooks
-       simpleUserHooks { preBuild = mkGmpDerivedConstants }
+       simpleUserHooks { buildHook = mkDerivedGmpConstants }
   where
-    mkGmpDerivedConstants :: Args -> BuildFlags -> IO HookedBuildInfo
-    mkGmpDerivedConstants _args (BuildFlags {}) = undefined
+    mkDerivedGmpConstants :: PackageDescription
+                          -> LocalBuildInfo
+                          -> UserHooks
+                          -> BuildFlags
+                          -> IO ()
+    mkDerivedGmpConstants pkg_descr lbi userHooks flags =
+        case lookupProgram gccProgram (withPrograms lbi) of
+            Just gcc -> do
+                withTempFile "src" "mkDerivedGmpConstants" $ \path _h -> do
+                    runProgram silent gcc
+                        [ "bin/mkDerivedGmpConstants.c", "-o", path]
+                    output <- rawSystemStdout silent path []
+                    writeFileAtomic ("cbits" </> "GmpDerivedConstants.h") output
+                buildHook simpleUserHooks pkg_descr lbi userHooks flags
+            Nothing -> die "Failed to find GCC!"
