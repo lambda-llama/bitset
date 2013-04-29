77235ee778ccce589466d835d724c9c3c47bf5c6
Sergei Lebedev <superbobry@gmail.com>
2013-04-30 01:19:59 +0400

    Removed 'toBig' conversion for small 'Integer' values

--- src/GHC/Integer/GMP/TypeExt.hs
+++ src/GHC/Integer/GMP/TypeExt.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE BangPatterns #-}
 
 module GHC.Integer.GMP.TypeExt
     ( popCountInteger
@@ -8,35 +9,34 @@ module GHC.Integer.GMP.TypeExt
     , clearBitInteger
     ) where
 
+import Data.Bits (popCount, testBit, clearBit)
+import GHC.Base (Int(..))
 import GHC.Integer.GMP.Internals (Integer(..))
 import GHC.Integer.GMP.Prim (int2Integer#)
-import GHC.Prim (Int#, (/=#), word2Int#, int2Word#, popCnt#)
+import GHC.Prim (Int#, (/=#))
 
 import GHC.Integer.GMP.PrimExt (popCountInteger#, testBitInteger#,
                                 setBitInteger#, clearBitInteger#)
 
 popCountInteger :: Integer -> Int#
-popCountInteger (S# i)   = (word2Int# (popCnt# (int2Word# i)))
+popCountInteger (S# i)   = let !(I# n) = popCount (I# i) in n
 popCountInteger (J# s d) = popCountInteger# s d
 {-# NOINLINE popCountInteger #-}
 
 testBitInteger :: Integer -> Int# -> Bool
-testBitInteger j@(S# _) i = testBitInteger (toBig j) i
+testBitInteger (S# j) i   = testBit (I# j) (I# i)
 testBitInteger (J# s d) i = testBitInteger# s d i /=# 0#
 {-# NOINLINE testBitInteger #-}
 
 setBitInteger :: Integer -> Int# -> Integer
-setBitInteger j@(S# _) i = setBitInteger (toBig j) i
+setBitInteger (S# j) i   =
+    let !(# s, d #) = int2Integer# j in setBitInteger (J# s d) i
 setBitInteger (J# s d) i =
-    let (# s', d' #) = setBitInteger# s d i in J# s' d'
+    let !(# s', d' #) = setBitInteger# s d i in J# s' d'
 {-# NOINLINE setBitInteger #-}
 
 clearBitInteger :: Integer -> Int# -> Integer
-clearBitInteger j@(S# _) i = clearBitInteger (toBig j) i
+clearBitInteger (S# j) i   = let !(I# j') = clearBit (I# j) (I# i) in S# j'
 clearBitInteger (J# s d) i =
-    let (# s', d' #) = clearBitInteger# s d i in J# s' d'
+    let !(# s', d' #) = clearBitInteger# s d i in J# s' d'
 {-# NOINLINE clearBitInteger #-}
-
-toBig :: Integer -> Integer
-toBig (S# i)     = case int2Integer# i of { (# s, d #) -> J# s d }
-toBig i@(J# _ _) = i
