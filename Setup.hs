0e1b86cc6c73e101c28a0bc7e6151b72bec58498
Sergei Lebedev <superbobry@gmail.com>
2013-04-29 18:47:06 +0400

    Fixed 'Text file busy' in 'Setup.hs'

--- Setup.hs
+++ Setup.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# OPTIONS_GHC -Wall -Werror #-}
 
+import System.Directory (removeFile)
 import System.FilePath ((</>))
 
 import Distribution.PackageDescription (PackageDescription)
@@ -10,8 +11,7 @@ import Distribution.Simple (UserHooks(..),
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
 import Distribution.Simple.Program (gccProgram, lookupProgram, runProgram)
 import Distribution.Simple.Setup (BuildFlags)
-import Distribution.Simple.Utils (die, withTempFile, rawSystemStdout,
-                                  writeFileAtomic)
+import Distribution.Simple.Utils (die, rawSystemStdout, writeFileAtomic)
 import Distribution.Verbosity (silent)
 
 main :: IO ()
@@ -25,11 +25,12 @@ main = defaultMainWithHooks
                           -> IO ()
     mkDerivedGmpConstants pkg_descr lbi userHooks flags =
         case lookupProgram gccProgram (withPrograms lbi) of
-            Just gcc -> do
-                withTempFile "src" "mkDerivedGmpConstants" $ \path _h -> do
+            Just gcc ->
+                let path = "src" </> "mkDerivedGmpConstants" in do
                     runProgram silent gcc
-                        [ "bin/mkDerivedGmpConstants.c", "-o", path]
+                        ["bin" </> "mkDerivedGmpConstants.c", "-o", path]
                     output <- rawSystemStdout silent path []
                     writeFileAtomic ("cbits" </> "GmpDerivedConstants.h") output
-                buildHook simpleUserHooks pkg_descr lbi userHooks flags
+                    removeFile path
+                    buildHook simpleUserHooks pkg_descr lbi userHooks flags
             Nothing -> die "Failed to find GCC!"
