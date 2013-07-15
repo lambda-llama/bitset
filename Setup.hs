847e897606b64ce655cbab2bb7d55ff91525e7e2
Sergei Lebedev <superbobry@gmail.com>
2013-07-16 01:01:31 +0400

    Fixed 'mkDerivedConstants' path on Windows
      See #9 for details

--- Setup.hs
+++ Setup.hs
@@ -13,6 +13,7 @@ import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
 import Distribution.Simple.Program (gccProgram, lookupProgram, runProgram)
 import Distribution.Simple.Setup (BuildFlags)
 import Distribution.Simple.Utils (die, rawSystemStdout)
+import Distribution.System (OS(..), buildOS)
 import Distribution.Verbosity (silent)
 
 main :: IO ()
@@ -27,7 +28,7 @@ main = defaultMainWithHooks
     mkDerivedGmpConstants pkg_descr lbi userHooks flags =
         case lookupProgram gccProgram (withPrograms lbi) of
             Just gcc ->
-                let path = "src" </> "mkDerivedGmpConstants" in do
+                let path = "src" </> exeName in do
                     runProgram silent gcc
                         ["bin" </> "mkDerivedGmpConstants.c", "-o", path]
                     output <- rawSystemStdout silent path []
@@ -35,3 +36,8 @@ main = defaultMainWithHooks
                     removeFile path
                     buildHook simpleUserHooks pkg_descr lbi userHooks flags
             Nothing -> die "Failed to find GCC!"
+      where
+        exeName :: FilePath
+        exeName = case buildOS of
+            Windows -> "mkDerivedGmpConstants.exe"
+            _       -> "mkDerivedGmpConstants"
